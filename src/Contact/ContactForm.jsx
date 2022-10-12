3e5b75240bdf4a63e7dd42de20c5d737b14300fb
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-12 23:37:49 +0200

    Testing font, adding a lot of css for contact

--- src/Contact/ContactForm.jsx
+++ src/Contact/ContactForm.jsx
@@ -4,27 +4,29 @@ import './ContactForm.css';
 export default function contactForm() {
   return (
     <div className="form_container">
-      <span>C'est parti !</span>
-      <span>Collaborons dans nos projets :</span>
-        <form 
-        name="contact" 
-        className="contact_form" 
-        method="POST" 
-        data-netlify="true" 
+      <div className='form_elements'>
+        <span>C'est parti !</span>
+        <span>Collaborons dans nos projets :</span>
+          <form 
+          name="contact" 
+          className="contact_form" 
+          method="POST" 
+          data-netlify="true" 
 
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <label for="name">Nom: </label>
-          <input type="text" name="name" />
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <label for="name">Nom: </label>
+            <input type="text" name="name" />
 
-          <label for="email">Email: </label>
-          <input type="email" name="email" />
-                
-          <label for="message">Message: </label>
-          <textarea name="message"></textarea>
+            <label for="email">Email: </label>
+            <input type="email" name="email" />
+                  
+            <label for="message">Message: </label>
+            <textarea name="message"></textarea>
 
-          <button type="submit">Envoyer</button>
-        </form>
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
     </div>
   )
 }
