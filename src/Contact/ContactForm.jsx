3a89a5e38486f94acc94b40798a3c77f5e2949e8
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-12 19:28:38 +0200

    Adding form contact

--- src/Contact/ContactForm.jsx
+++ src/Contact/ContactForm.jsx
@@ -4,11 +4,23 @@ import './ContactForm.css';
 export default function contactForm() {
   return (
     <div className="form_container">
-        <form name="contact" method="POST" data-netlify="true">
-                <label>Your Name: <input type="text" name="name" /></label>
-                <label>Your Email: <input type="email" name="email" /></label>
-                <label>Message: <textarea name="message"></textarea></label>
-                <button type="submit">Send</button>
+        <form 
+        name="contact" 
+        className="contact_form" 
+        method="POST" 
+        data-netlify="true" onSubmit="submit"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <label for="name">Nom: </label>
+          <input type="text" name="name" />
+
+          <label for="email">Email: </label>
+          <input type="email" name="email" />
+                
+          <label for="message">Message: </label>
+          <textarea name="message"></textarea>
+
+          <button type="submit">Envoyer</button>
         </form>
     </div>
   )
