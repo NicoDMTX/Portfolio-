df18600216bfb2297276c2350bb4814c94c0b86a
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-13 16:14:41 +0200

    adding switch in app about links navbar, adding home file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
 import Navbar from './Navbar/Navbar'
 import ContactForm from './Contact/ContactForm'
+import Home from './Home/Home'
 
 function App() {
-  
+  let component
+  switch (window.location.pathname) {
+    case "/contact":
+      component = <ContactForm />
+      break;
+    case "/home":
+      component = <Home />
+      break;
+  }
   return (
     <div className="App">
       <Navbar />
-      <ContactForm />
-      
+      {component}
     </div>
   )
 }
---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className='home_container'>
+        <h1 className="title">Nicolas Demontoux</h1>
+        <h2 className="title">Développeur junior</h2>
+    </div>
+  )
+}
