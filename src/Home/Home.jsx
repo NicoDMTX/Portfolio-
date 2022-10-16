651289d5a51843fc7c3e6f19cac1777222f3cb93
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-17 00:40:02 +0200

    Adding png and svg for home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,9 +3,24 @@ import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.home_container}>
-        <h1 className={styles.title}>Nicolas Demontoux</h1>
-        <h2 className={styles.title}>Développeur junior</h2>
+    <>
+    <div className={styles.big_container}>
+      <div className={styles.bitmoji_container}>
+          <img src="/public/home_bitmoji3.png" className={styles.home_bitmoji}></img>
+      </div>
+
+      <div className={styles.home_container}>
+          <h1 className={styles.title}>Nicolas Demontoux</h1>
+          <h2 className={styles.title}>Développeur junior</h2>
+      </div>
     </div>
+    <div className={styles.img_sizer}>
+      <div className={styles.img_container}></div>
+    </div>
+    
+    
+    
+    </>
+    
   )
 }
